8c6d544dc01de2e3e2a87014d8581a992041474f
sgiehl <stefan@piwik.org>
2014-08-19 22:54:06 +0200

    added some more tests

--- Tests/DeviceDetectorTest.php
+++ Tests/DeviceDetectorTest.php
@@ -7,12 +7,19 @@
  */
 namespace DeviceDetector\Tests;
 
+use DeviceDetector\Cache\CacheMemcache;
 use DeviceDetector\DeviceDetector;
 use DeviceDetector\Parser\Device\DeviceParserAbstract;
 use \Spyc;
 
 class DeviceDetectorTest extends \PHPUnit_Framework_TestCase
 {
+    public function testCacheSetAndGet()
+    {
+        $dd = new DeviceDetector();
+        $dd->setCache(new CacheMemcache());
+        $this->assertInstanceOf('DeviceDetector\\Cache\\CacheMemcache', $dd->getCache());
+    }
 
     /**
      * @dataProvider getFixtures
@@ -83,4 +90,27 @@ class DeviceDetectorTest extends \PHPUnit_Framework_TestCase
         $fixtures = \Spyc::YAMLLoad($fixturesPath);
         return array_map(function($elem) {return array($elem);}, $fixtures);
     }
+
+    /**
+     * @dataProvider getUserAgents
+     */
+    public function testTypeMethods($useragent, $isBot, $isMobile, $isDesktop)
+    {
+        $dd = new DeviceDetector($useragent);
+        $dd->parse();
+        $this->assertEquals($isBot, $dd->isBot());
+        $this->assertEquals($isMobile, $dd->isMobile());
+        $this->assertEquals($isDesktop, $dd->isDesktop());
+    }
+
+    public function getUserAgents()
+    {
+        return array(
+            array('Googlebot/2.1 (http://www.googlebot.com/bot.html)', true, false, false),
+            array('Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.136 Mobile Safari/537.36', false, true, false),
+            array('Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)', false, false, true),
+            array('Mozilla/3.01 (compatible;)', false, false, false),
+        );
+    }
+
 }
