9a515ccc08e6a90ae117e346f3e8c705f66f0245
Matthieu Aubry <matt@piwik.org>
2014-09-26 13:58:59 +1200

    add silent fail in front of opcache_ refs https://github.com/piwik/piwik/pull/6303/files

--- Cache/CacheFile.php
+++ Cache/CacheFile.php
@@ -119,7 +119,7 @@ class CacheFile extends CacheStatic implements CacheInterface
         if (function_exists('opcache_invalidate')
             && is_file($filepath)
         ) {
-            opcache_invalidate($filepath, $force = true);
+            @opcache_invalidate($filepath, $force = true);
         }
     }
 }
