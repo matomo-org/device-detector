7a4790605319744852b4891c5442ecc178694899
sgiehl <stefan@piwik.org>
2014-12-02 21:51:39 +0100

    small changes

--- Tests/DeviceDetectorTest.php
+++ Tests/DeviceDetectorTest.php
@@ -44,7 +44,6 @@ class DeviceDetectorTest extends \PHPUnit_Framework_TestCase
     {
         $dd = new DeviceDetector('');
         $dd->parse();
-        $dd->parse();
     }
 
     public function testParseInvalidUA()
