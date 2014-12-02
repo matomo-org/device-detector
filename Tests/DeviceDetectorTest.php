b73c5d654cb62951503396bce2f30223837bb2b7
sgiehl <stefan@piwik.org>
2014-12-02 21:54:05 +0100

    cc

--- Tests/DeviceDetectorTest.php
+++ Tests/DeviceDetectorTest.php
@@ -44,6 +44,7 @@ class DeviceDetectorTest extends \PHPUnit_Framework_TestCase
     {
         $dd = new DeviceDetector('');
         $dd->parse();
+        $dd->parse(); // call second time complete code coverage
     }
 
     public function testParseInvalidUA()
