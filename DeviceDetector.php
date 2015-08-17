d1804795407c4c71a0ce33667a4ce84a9f9fcd0d
sgiehl <stefan@piwik.org>
2015-08-17 23:41:00 +0200

    next version

--- DeviceDetector.php
+++ DeviceDetector.php
@@ -47,7 +47,7 @@ class DeviceDetector
     /**
      * Current version number of DeviceDetector
      */
-    const VERSION = '3.3.0';
+    const VERSION = '3.4.0';
 
     /**
      * Holds all registered client types
