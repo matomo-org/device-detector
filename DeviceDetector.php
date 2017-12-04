caf2d15349caffd94a8cfe46edf4061fbc99f70b
sgiehl <stefan@piwik.org>
2017-12-04 10:56:31 +0100

    new release 3.9.1

--- DeviceDetector.php
+++ DeviceDetector.php
@@ -49,7 +49,7 @@ class DeviceDetector
     /**
      * Current version number of DeviceDetector
      */
-    const VERSION = '3.9.0';
+    const VERSION = '3.9.1';
 
     /**
      * Holds all registered client types
