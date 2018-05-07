67e96595cd7649b7967533053fcbfbe02d5c55a3
Stefan Giehl <stefan@piwik.org>
2018-05-07 21:33:29 +0200

    new version

--- DeviceDetector.php
+++ DeviceDetector.php
@@ -50,7 +50,7 @@ class DeviceDetector
     /**
      * Current version number of DeviceDetector
      */
-    const VERSION = '3.10.1';
+    const VERSION = '3.10.2';
 
     /**
      * Holds all registered client types
