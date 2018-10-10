b57287ef7adc9f400c3a3a4f3863e1c4f7994ea2
sanchezzzhak <sanchezzzhak@ya.ru>
2018-10-10 19:55:49 +0300

    #5833 add detect tv consoles  AppleTv, Savio

--- Parser/Device/DeviceParserAbstract.php
+++ Parser/Device/DeviceParserAbstract.php
@@ -355,6 +355,7 @@ abstract class DeviceParserAbstract extends ParserAbstract
         'S7' => 'Smartisan',
         'S4' => 'Star',
         'S8' => 'STK',
+        'S9' => 'Savio',
         'ST' => 'Storex',
         'S2' => 'Stonex',
         'S3' => 'SunVan',
