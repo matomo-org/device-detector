a12ff1436301554ef308caf132f5c49010d0f705
Guillaume Malleret <guillaume.malleret@flex-multimedia.com>
2018-11-29 11:16:37 +0100

    Add Pecl YAML parser

---
+++ Yaml/Pecl.php
@@ -0,0 +1,13 @@
+<?php
+namespace DeviceDetector\Yaml;
+
+class Pecl implements Parser
+{
+    public function parseFile($file)
+    {
+        if(function_exists(yaml_parse_file) === false)
+            throw new \Exception('Pecl YAML extension is not installed');
+        
+		return yaml_parse_file($file);
+    }
+}
