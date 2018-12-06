f3aba71e36ed9f7e3bc3fac809fa85088eb97945
mallsbill <mallsbill@users.noreply.github.com>
2018-12-06 23:03:33 +0100

    Add Pecl YAML parser (#5876)
    * Add Pecl YAML parser
    
    * add ext-yaml to suggest
    
    * fix indentation
    
    * Add documentation and braces

---
+++ Yaml/Pecl.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Device Detector - The Universal Device Detection library for parsing User Agents
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL v3 or later
+ */
+namespace DeviceDetector\Yaml;
+
+use Exception;
+
+/**
+ * Class Pecl
+ * 
+ * Parses a YAML file with LibYAML library
+ * 
+ * @package DeviceDetector\Yaml
+ * @see http://php.net/manual/en/function.yaml-parse-file.php
+ */
+class Pecl implements Parser
+{
+    /**
+     * @param string $file The path to the YAML file to be parsed
+     * 
+     * @return mixed The YAML converted to a PHP value or FALSE on failure
+     * @throws Exception If the YAML extension is not installed
+     */
+    public function parseFile($file)
+    {
+        if(function_exists(yaml_parse_file) === false)
+        {
+            throw new Exception('Pecl YAML extension is not installed');
+        }
+        
+        return yaml_parse_file($file);
+    }
+}
