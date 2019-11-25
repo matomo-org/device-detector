4b265874860fd717b55528ec6f613df0718c7ed2
sanchezzzhak <sanchezzzhak@ya.ru>
2019-11-25 20:54:21 +0300

    feat(devices): added detect iZotron tables

--- Parser/Device/DeviceParserAbstract.php
+++ Parser/Device/DeviceParserAbstract.php
@@ -283,6 +283,7 @@ abstract class DeviceParserAbstract extends ParserAbstract
         'IV' => 'Inverto',
         'I3' => 'Impression',
         'IZ' => 'iTel',
+        'I9' => 'iZotron',
         'JA' => 'JAY-Tech',
         'JI' => 'Jiayu',
         'JO' => 'Jolla',
