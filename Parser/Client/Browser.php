f4926d4b1c70d1b3e850fa08441c58df7e68f279
liviuconcioiu <liviu.concioiu@gmail.com>
2019-11-29 01:19:23 +0100

    Add Firefox Focus to mobile only browsers

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -279,7 +279,7 @@ class Browser extends ClientParserAbstract
      * @var array
      */
     protected static $mobileOnlyBrowsers = array(
-        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'MN', 'SU', 'M1'
+        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'MN', 'SU', 'M1', 'FK'
     );
 
     /**
