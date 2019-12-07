b9cc41a8e27b88810a9887c5111da68cdb666183
liviuconcioiu <liviu.concioiu@gmail.com>
2019-12-07 22:24:24 +0100

    Adds detection for DU Browser browser
    ref #5702

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -84,6 +84,7 @@ class Browser extends ClientParserAbstract
         'DO' => 'Dorado',
         'DL' => 'Dooble',
         'DI' => 'Dillo',
+        'DU' => 'DU Browser',
         'DD' => 'DuckDuckGo Privacy Browser',
         'EC' => 'Ecosia',
         'EI' => 'Epic',
@@ -264,7 +265,7 @@ class Browser extends ClientParserAbstract
     protected static $browserFamilies = array(
         'Android Browser'    => array('AN', 'MU'),
         'BlackBerry Browser' => array('BB'),
-        'Baidu'              => array('BD', 'BS'),
+        'Baidu'              => array('BD', 'BS', 'DU'),
         'Amiga'              => array('AV', 'AW'),
         'Chrome'             => array('CH', 'BA', 'BR', 'CC', 'CD', 'CM', 'CI', 'CF', 'CN', 'CR', 'CP', 'DD', 'IR', 'RM', 'AO', 'TS', 'VI', 'PT', 'AS', 'TB', 'AD', 'SB', 'WP', 'I3', 'CV', 'WH', 'SZ', 'QW', 'LF', 'KW', '2B', 'CE', 'EC', 'MT', 'MS', 'HA', 'OC', 'SW', 'CL', 'BM', 'KN', 'TA', 'FA', 'M1', 'UB', 'SU', 'AH', 'EU'),
         'Firefox'            => array('FF', 'FE', 'FM', 'SX', 'FB', 'PX', 'MB', 'EI', 'WF', 'CU', 'TF', 'QM', 'FR', 'I4', 'GZ', 'MO', 'F1', 'BI', 'TO', 'MN'),
@@ -284,7 +285,7 @@ class Browser extends ClientParserAbstract
      * @var array
      */
     protected static $mobileOnlyBrowsers = array(
-        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'AH', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'MN', 'SU', 'M1', 'FK', 'UT', 'UM', 'FX', 'MZ', 'EU'
+        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'AH', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'MN', 'SU', 'M1', 'FK', 'UT', 'UM', 'FX', 'MZ', 'EU', 'DU'
     );
 
     /**
