6ae4552e716faaf76c5dab33ebf66e7edb33b1b6
liviuconcioiu <liviu.concioiu@gmail.com>
2019-12-11 09:17:11 +0100

    Adds detection for Splash browser (#6114)

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -207,6 +207,7 @@ class Browser extends ClientParserAbstract
         'SL' => 'Sleipnir',
         'SN' => 'Snowshoe',
         'SO' => 'Sogou Mobile Browser',
+        'S2' => 'Splash',
         'SI' => 'Sputnik Browser',
         'SR' => 'Sunrise',
         'SP' => 'SuperBird',
