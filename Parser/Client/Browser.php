2315ff41b473434de0e8da45b266736383899b63
liviuconcioiu <liviu.concioiu@gmail.com>
2019-12-21 23:07:55 +0100

    Adds detection for UC Browser Turbo browser (#6133)
    * Adds detection for UC Browser Turbo browser
    
    * Add test
    
    * Add test
    
    * Update smartphone-6.yml
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -238,6 +238,7 @@ class Browser extends ClientParserAbstract
         'UB' => 'UBrowser',
         'UC' => 'UC Browser',
         'UM' => 'UC Browser Mini',
+        'UT' => 'UC Browser Turbo',
         'UZ' => 'Uzbl',
         'VI' => 'Vivaldi',
         'VV' => 'vivo Browser',
@@ -286,7 +287,7 @@ class Browser extends ClientParserAbstract
      * @var array
      */
     protected static $mobileOnlyBrowsers = array(
-        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'CB', 'MZ', 'UM', 'FK', 'FX', 'WI', 'MN', 'M1', 'AH', 'SU', 'EU', 'EZ'
+        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'CB', 'MZ', 'UM', 'FK', 'FX', 'WI', 'MN', 'M1', 'AH', 'SU', 'EU', 'EZ', 'UT'
     );
 
     /**
