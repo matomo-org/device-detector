734175653a9f30c02e58d352b19be83694474aba
liviuconcioiu <liviu.concioiu@gmail.com>
2019-12-29 20:14:00 +0100

    Adds detection for START Internet Browser (#6145)
    * Adds detection for START Internet Browser
    
    * Fix
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -230,6 +230,7 @@ class Browser extends ClientParserAbstract
         'SR' => 'Sunrise',
         'SP' => 'SuperBird',
         'SU' => 'Super Fast Browser',
+        'S0' => 'START Internet Browser',
         'ST' => 'Streamy',
         'SX' => 'Swiftfox',
         'SZ' => 'Seznam Browser',
@@ -291,7 +292,7 @@ class Browser extends ClientParserAbstract
      * @var array
      */
     protected static $mobileOnlyBrowsers = array(
-        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'CB', 'MZ', 'UM', 'FK', 'FX', 'WI', 'MN', 'M1', 'AH', 'SU', 'EU', 'EZ', 'UT', 'DT'
+        '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'SA', 'SB', 'FR', 'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'CB', 'MZ', 'UM', 'FK', 'FX', 'WI', 'MN', 'M1', 'AH', 'SU', 'EU', 'EZ', 'UT', 'DT', 'S0'
     );
 
     /**
