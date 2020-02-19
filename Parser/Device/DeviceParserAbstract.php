7dbbec2885430f0e5f91eb7d8164da284cc6ab15
sanchezzzhak <sanchezzzhak@ya.ru>
2020-02-19 18:22:42 +0300

    feat (device) add new brand AllDocube && detect tablet model - M5XS (T1001XS)

--- Parser/Device/DeviceParserAbstract.php
+++ Parser/Device/DeviceParserAbstract.php
@@ -77,6 +77,7 @@ abstract class DeviceParserAbstract extends ParserAbstract
         'AK' => 'Akai',
         '1A' => 'Alba',
         'AL' => 'Alcatel',
+        '3A' => 'AllDocube',
         'A2' => 'Allview',
         'A7' => 'Allwinner',
         'A1' => 'Altech UEC',
