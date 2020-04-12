99d9396476cdfbae3a2c172e6f787e1b291ebe56
sanchezzzhak <sanchezzzhak@yandex.ru>
2020-04-12 04:01:56 +0300

    feat(device) detect new brand Soyes models: 6S, 7S

--- Parser/Device/DeviceParserAbstract.php
+++ Parser/Device/DeviceParserAbstract.php
@@ -530,6 +530,7 @@ abstract class DeviceParserAbstract extends ParserAbstract
         'SC' => 'Smartfren',
         'SO' => 'Sony',
         'OI' => 'Sonim',
+        '8S' => 'Soyes',
         'SP' => 'Spice',
         '6S' => 'Spectrum',
         '5S' => 'Sunvell',
