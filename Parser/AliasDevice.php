b5c0f250086b02883e98c39db78020b4399c7edc
sanchezzzhak <sanchezzzhak@yandex.ru>
2020-05-12 14:59:17 +0300

    feat(device) remove brand result alias devices;

--- Parser/AliasDevice.php
+++ Parser/AliasDevice.php
@@ -8,21 +8,27 @@
 
 namespace DeviceDetector\Parser;
 
+use DeviceDetector\DeviceDetector;
+use DeviceDetector\Parser\Device\DeviceParserAbstract;
+
 /**
  * Class AliasDevice
  * @package DeviceDetector\Parser
  * @uses ```php
  *      use DeviceDetector\Parser\AliasDevice;
  *
- *      $userAgent = '';
+ *      $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
  *      $parser = new AliasDevice($userAgent);
- *
+ *      $result = $parser->parse();
+ *      var_dump($result);
  * ```
  */
 class AliasDevice extends ParserAbstract
 {
     protected $fixtureFile = 'regexes/alias_devices.yml';
     protected $parserName = 'alias_device';
+
+    private $brandReplaceRegexp = null;
 
     public function parse()
     {
@@ -41,9 +47,32 @@ class AliasDevice extends ParserAbstract
         }
 
         $name = $this->buildByMatch($aliasDeviceRegex['name'], $matches);
+        $name = preg_replace($this->getBrandReplaceRegexp(), '', $name);
         $name = trim($name);
 
         return compact('name');
+    }
+
+    /**
+     * @return null|string
+     */
+    private function getBrandReplaceRegexp()
+    {
+        if (empty($this->brandReplaceRegexp)) {
+            $cacheKey = sprintf('DeviceDetector-%s-brands-regexp', DeviceDetector::VERSION);
+            $this->brandReplaceRegexp = $this->getCache()->fetch($cacheKey);
+            if (empty($this->brandReplaceRegexp)) {
+
+                $escapeeChars = ['+' => '\+', '.' => '\.'];
+                $brands = implode('|', array_values(DeviceParserAbstract::$deviceBrands));
+                $brands = str_replace(array_keys($escapeeChars), array_values($escapeeChars), $brands);
+                $pattern = sprintf('#([ _-]?(?:%s)[ _-]?)#i', $brands);
+
+                $this->brandReplaceRegexp = $pattern;
+                $this->getCache()->save($cacheKey, $this->brandReplaceRegexp);
+            }
+        }
+        return $this->brandReplaceRegexp;
     }
 
 }
