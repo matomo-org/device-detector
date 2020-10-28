4a6d6101004127f3802685f309407f74b4dbb784
Stefan Giehl <stefan@matomo.org>
2020-10-28 20:49:10 +0100

    adds missing ;

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -914,7 +914,7 @@ abstract class AbstractDeviceParser extends AbstractParser
      */
     public static function getShortCode(string $brand): string
     {
-        return \array_search($brand, self::$deviceBrands) ?: ''
+        return \array_search($brand, self::$deviceBrands) ?: '';
     }
 
     /**
