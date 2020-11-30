8129a83be7ca9ac3ac7bd0b6fce85f22e9f0d499
liviuconcioiu <liviu.concioiu@gmail.com>
2020-11-30 13:14:22 +0100

    Adds detection for Ghostery Privacy Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -131,6 +131,7 @@ class Browser extends AbstractClientParser
         'FN' => 'Fireweb Navigator',
         'FU' => 'FreeU',
         'GA' => 'Galeon',
+        'GH' => 'Ghostery Privacy Browser',
         'GB' => 'Glass Browser',
         'GE' => 'Google Earth',
         'GO' => 'GOG Galaxy',
@@ -337,7 +338,7 @@ class Browser extends AbstractClientParser
             'FF', 'FE', 'FM', 'SX', 'FB', 'PX', 'MB', 'EI', 'WF',
             'CU', 'TF', 'QM', 'FR', 'I4', 'GZ', 'MO', 'F1', 'BI',
             'MN', 'BH', 'TO', 'OS', 'MY', 'FY', 'AX', 'C0', 'LH',
-            'S5', 'ZV', 'IW',
+            'S5', 'ZV', 'IW', 'GH',
         ],
         'Internet Explorer'  => ['IE', 'IM', 'PS', 'CZ'],
         'Konqueror'          => ['KO'],
@@ -357,7 +358,7 @@ class Browser extends AbstractClientParser
     protected static $mobileOnlyBrowsers = [
         '36', 'OC', 'PU', 'SK', 'MF', 'OI', 'OM', 'DD', 'DB', 'ST', 'BL', 'IV', 'FM', 'C1', 'AL', 'SA', 'SB', 'FR',
         'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'CB', 'MZ', 'UM', 'FK', 'FX', 'WI', 'MN', 'M1', 'AH', 'SU', 'EU', 'EZ',
-        'UT', 'DT', 'S0', 'QU', 'YN', 'JB',
+        'UT', 'DT', 'S0', 'QU', 'YN', 'JB', 'GH',
     ];
 
     /**
