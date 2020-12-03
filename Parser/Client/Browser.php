56d5e4b099e9fe7c459e3c0f2823fac933f0ea5b
liviuconcioiu <liviu.concioiu@gmail.com>
2020-12-03 17:32:31 +0100

    Adds detection for PlayFree Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -235,6 +235,7 @@ class Browser extends AbstractClientParser
         'PA' => 'Palmscape',
         'PX' => 'Phoenix',
         'PB' => 'Phoenix Browser',
+        'PF' => 'PlayFree Browser',
         'PO' => 'Polaris',
         'PT' => 'Polarity',
         'PI' => 'PrivacyWall',
@@ -334,7 +335,7 @@ class Browser extends AbstractClientParser
             'CL', 'SU', 'EU', 'UB', 'LO', 'VG', 'TV', 'A0', '1B',
             'S4', 'EE', 'AE', 'VM', 'O0', 'TG', 'GB', 'SY', 'HH',
             'YJ', 'LL', 'TU', 'XV', 'C2', 'QU', 'YN', 'JB', 'MR',
-            'S6', '7S', 'NM', 'PB',
+            'S6', '7S', 'NM', 'PB', 'PF',
         ],
         'Firefox'            => [
             'FF', 'FE', 'FM', 'SX', 'FB', 'PX', 'MB', 'EI', 'WF',
