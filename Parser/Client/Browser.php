4e1b2655ae8e90a2736bc54da600b29bce18c12b
liviuconcioiu <liviu.concioiu@gmail.com>
2021-01-21 21:38:31 +0100

    Adds detection for Perfect Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -241,6 +241,7 @@ class Browser extends AbstractClientParser
         'PU' => 'Puffin',
         'PW' => 'Palm WebPro',
         'PA' => 'Palmscape',
+        'PE' => 'Perfect Browser',
         'PH' => 'Phantom Browser',
         'PX' => 'Phoenix',
         'PB' => 'Phoenix Browser',
@@ -382,7 +383,7 @@ class Browser extends AbstractClientParser
         'WP', 'HA', 'NX', 'HU', 'VV', 'RE', 'CB', 'MZ', 'UM',
         'FK', 'FX', 'WI', 'MN', 'M1', 'AH', 'SU', 'EU', 'EZ',
         'UT', 'DT', 'S0', 'QU', 'YN', 'JB', 'GH', 'PI', 'SG',
-        'KD',
+        'KD', 'PE',
     ];
 
     /**
