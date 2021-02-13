a07038b3d4c880ca5e100b99a2136e5e1e5dcbae
liviuconcioiu <liviu.concioiu@gmail.com>
2021-02-13 10:31:52 +0100

    Adds detection for Helio browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -143,6 +143,7 @@ class Browser extends AbstractClientParser
         'GE' => 'Google Earth',
         'GO' => 'GOG Galaxy',
         'HA' => 'Hawk Turbo Browser',
+        'HE' => 'Helio',
         'HO' => 'hola! Browser',
         'HJ' => 'HotJava',
         'HU' => 'Huawei Browser',
@@ -362,7 +363,7 @@ class Browser extends AbstractClientParser
             'S4', 'EE', 'AE', 'VM', 'O0', 'TG', 'GB', 'SY', 'HH',
             'YJ', 'LL', 'TU', 'XV', 'C2', 'QU', 'YN', 'JB', 'MR',
             'S6', '7S', 'NM', 'PB', 'UR', 'NR', 'SG', 'S8', 'C3',
-            'SS', 'O2', 'SV', 'TR', 'CW',
+            'SS', 'O2', 'SV', 'TR', 'CW', 'HE',
         ],
         'Firefox'            => [
             'FF', 'FE', 'FM', 'SX', 'FB', 'PX', 'MB', 'EI', 'WF',
