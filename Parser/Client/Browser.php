ea65e1bc214ce3a8e682ff154d8a45116d81295f
liviuconcioiu <liviu.concioiu@gmail.com>
2021-02-13 10:16:35 +0100

    Add to Chrome family

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -363,7 +363,7 @@ class Browser extends AbstractClientParser
             'S4', 'EE', 'AE', 'VM', 'O0', 'TG', 'GB', 'SY', 'HH',
             'YJ', 'LL', 'TU', 'XV', 'C2', 'QU', 'YN', 'JB', 'MR',
             'S6', '7S', 'NM', 'PB', 'UR', 'NR', 'SG', 'S8', 'C3',
-            'SS', 'O2', 'SV', 'TR', 'CW',
+            'SS', 'O2', 'SV', 'TR', 'CW', 'TH',
         ],
         'Firefox'            => [
             'FF', 'FE', 'FM', 'SX', 'FB', 'PX', 'MB', 'EI', 'WF',
