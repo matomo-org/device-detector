df39eb4bc2bde19418acde2b3e2ccbf5c67aa26c
liviuconcioiu <liviu.concioiu@gmail.com>
2021-02-14 13:45:00 +0100

    Adds detection for JavaFX WebView

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -162,6 +162,7 @@ class Browser extends AbstractClientParser
         'IR' => 'Iron',
         'JB' => 'Japan Browser',
         'JS' => 'Jasmine',
+        'JA' => 'JavaFX',
         'JI' => 'Jig Browser',
         'JP' => 'Jig Browser Plus',
         'JO' => 'Jio Browser',
