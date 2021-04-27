fe924b93b63527ecb1c08c28d4262c5bf44ca592
Tutik Alexsandr <sanchezzzhak@yandex.ru>
2021-04-27 23:22:05 +0300

    fix: PHPCS

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -387,7 +387,7 @@ class Browser extends AbstractClientParser
             'S6', '7S', 'NM', 'PB', 'UR', 'NR', 'SG', 'S8', 'C3',
             'SS', 'O2', 'SV', 'TR', 'CW', 'GI', 'VE', 'AC', 'FS',
             'CG', 'DR', 'C4', 'HS', 'DA', 'C5', 'C6', 'DG', 'CG',
-            'H2'
+            'H2',
         ],
         'Firefox'            => [
             'FF', 'FE', 'FM', 'SX', 'FB', 'PX', 'MB', 'EI', 'WF',
