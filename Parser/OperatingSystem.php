8a3bc68ce7dd09571e1be04eb10f4e118cb6453b
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2021-05-29 23:33:11 +0200

    Adds detection for HiOS operating system and adds detection for Tecno Mobile Camon 16 Premier

--- Parser/OperatingSystem.php
+++ Parser/OperatingSystem.php
@@ -70,6 +70,7 @@ class OperatingSystem extends AbstractParser
         'HPX' => 'HP-UX',
         'HAI' => 'Haiku OS',
         'HAS' => 'HasCodingOS',
+        'HOS' => 'HiOS',
         'IRI' => 'IRIX',
         'INF' => 'Inferno',
         'KOS' => 'KaiOS',
@@ -144,7 +145,7 @@ class OperatingSystem extends AbstractParser
      * @var array
      */
     protected static $osFamilies = [
-        'Android'               => ['AND', 'CYN', 'FIR', 'REM', 'RZD', 'MLD', 'MCD', 'YNS', 'GRI'],
+        'Android'               => ['AND', 'CYN', 'FIR', 'REM', 'RZD', 'MLD', 'MCD', 'YNS', 'GRI', 'HOS'],
         'AmigaOS'               => ['AMG', 'MOR'],
         'Apple TV'              => ['ATV'],
         'BlackBerry'            => ['BLB', 'QNX'],
