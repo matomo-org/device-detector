9789c22e7169f006cbbcb7431a766f3c50ec0244
sanchezzzhak <sanchezzzhak@ya.ru>
2021-06-18 17:02:50 +0300

    feat(test) create new test ShellTvTest

---
+++ Tests/Parser/Device/ShellTvTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Device Detector - The Universal Device Detection library for parsing User Agents
+ *
+ * @link https://matomo.org
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL v3 or later
+ */
+
+namespace DeviceDetector\Tests\Parser\Device;
+
+use DeviceDetector\Parser\Device\ShellTv;
+use PHPUnit\Framework\TestCase;
+
+class ShellTvTest extends TestCase
+{
+    public function testIsHbbTv(): void
+    {
+        $dd = new ShellTv();
+        $dd->setUserAgent('Leff Shell LC390TA2A');
+        $this->assertEquals($dd->isShellTv(), true);
+        $dd->setUserAgent('Leff Shell');
+        $this->assertEquals($dd->isShellTv(), false);
+    }
+}
