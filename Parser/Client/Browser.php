dfafb395ba5785311dae7315614ab8d4aa4bb803
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2021-10-29 02:26:08 +0200

    Adds detection for Landis+Gyr AIM Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -199,6 +199,7 @@ class Browser extends AbstractClientParser
         'KZ' => 'Kazehakase',
         'LB' => 'Cheetah Browser',
         'LA' => 'Lagatos Browser',
+        'LD' => 'Landis+Gyr AIM Browser',
         'LF' => 'LieBaoFast',
         'LG' => 'LG Browser',
         'LH' => 'Light',
@@ -405,7 +406,7 @@ class Browser extends AbstractClientParser
             'SG', 'SS', 'SU', 'SV', 'SW', 'SY', 'SZ', 'T1', 'TA',
             'TB', 'TG', 'TR', 'TS', 'TU', 'TV', 'UB', 'UR', 'VE',
             'VG', 'VI', 'VM', 'WP', 'WH', 'XV', 'YJ', 'YN', 'Y8',
-            'FH', 'B1', 'NU', 'BO', 'HB', 'PC', 'LA',
+            'FH', 'B1', 'NU', 'BO', 'HB', 'PC', 'LA', 'LD',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
