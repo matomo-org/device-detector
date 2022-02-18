430408fb561d9c09fa446cb79bf48096b0f2acde
sanchezzzhak <sanchezzzhak@ya.ru>
2022-02-18 18:47:31 +0300

    feat(test) added tests from opera and edge

--- Tests/ClientHintsTest.php
+++ Tests/ClientHintsTest.php
@@ -31,11 +31,13 @@ class ClientHintsTest extends TestCase
                     'Sec-CH-UA-Platform-Version' => '14.0.0',
                 ],
                 'fixture'    => [
-                    'name'       => 'Windows',
-                    'short_name' => 'WIN',
-                    'version'    => '11',
-                    'platform'   => 'x64',
-                    'family'     => 'Windows',
+                    'os' => [
+                        'name'       => 'Windows',
+                        'short_name' => 'WIN',
+                        'version'    => '11',
+                        'platform'   => 'x64',
+                        'family'     => 'Windows',
+                    ]
                 ],
             ], [
                 'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.3',
@@ -45,23 +47,61 @@ class ClientHintsTest extends TestCase
                     'Sec-CH-UA-Platform' => 'Windows',
                 ],
                 'fixture'    => [
-                    'name'       => 'Windows',
-                    'short_name' => 'WIN',
-                    'version'    => '10',
-                    'platform'   => 'x64',
-                    'family'     => 'Windows',
+                    'os' => [
+                        'name'       => 'Windows',
+                        'short_name' => 'WIN',
+                        'version'    => '10',
+                        'platform'   => 'x64',
+                        'family'     => 'Windows',
+                    ],
                 ],
             ], [
                 'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.3',
                 'headers'    => [],
                 'fixture'    => [
-                    'name'       => 'Windows',
-                    'short_name' => 'WIN',
-                    'version'    => '10',
-                    'platform'   => 'x64',
-                    'family'     => 'Windows',
+                    'os' => [
+                        'name'       => 'Windows',
+                        'short_name' => 'WIN',
+                        'version'    => '10',
+                        'platform'   => 'x64',
+                        'family'     => 'Windows',
+                    ],
                 ],
-            ],
+            ], [
+                'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44',
+                'headers' => [
+                    'sec-ch-ua' => '" Not A;Brand";v="99", "Chromium";v="95", "Microsoft Edge";v="95"',
+                    'sec-ch-ua-mobile' => '?0',
+                    'sec-ch-ua-platform' => 'Windows',
+                    'sec-ch-ua-platform-version' => '14.0.0',
+                ],
+                'fixture'    => [
+                    'os' => [
+                        'name' => 'Windows',
+                        'short_name' => 'WIN',
+                        'version' => '11',
+                        'platform' => 'x64',
+                        'family' => 'Windows',
+                    ]
+                ],
+            ], [
+                'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 OPR/83.0.4254.27',
+                'headers' => [
+                    'sec-ch-ua'=> '"Opera";v="83", " Not;A Brand";v="99", "Chromium";v="98"',
+                    'sec-ch-ua-mobile' => '?0',
+                    'sec-ch-ua-platform'=> 'Windows',
+                    'sec-ch-ua-platform-version'=> '14.0.0',
+                ],
+                'fixture'    => [
+                    'os' => [
+                        'name' => 'Windows',
+                        'short_name' => 'WIN',
+                        'version' => '11',
+                        'platform' => 'x64',
+                        'family' => 'Windows',
+                    ]
+                ],
+            ]
         ];
     }
 
@@ -74,6 +114,6 @@ class ClientHintsTest extends TestCase
         $ch = ClientHints::factory($headers);
         $dd = new DeviceDetector($useragent, $ch);
         $dd->parse();
-        $this->assertEquals($dd->getOs(), $fixture);
+        $this->assertEquals($dd->getOs(), $fixture['os']);
     }
 }
