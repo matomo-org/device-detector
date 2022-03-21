7768c205aed390d8092e624b0a1e7d84d44a77af
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-03-21 19:55:12 +0100

    Improves detection for Monument Browser
    ref #7039

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -493,7 +493,7 @@ class Browser extends AbstractClientParser
         'PE', 'QU', 'RE', 'S0', 'S7', 'SA', 'SB', 'SG', 'SK',
         'ST', 'SU', 'T1', 'UH', 'UM', 'UT', 'VE', 'VV', 'WI',
         'WP', 'YN', 'IO', 'IS', 'HQ', 'RW', 'HI', 'NA', 'BW',
-        'YO', 'PK',
+        'YO', 'PK', 'MR',
     ];
 
     /**
