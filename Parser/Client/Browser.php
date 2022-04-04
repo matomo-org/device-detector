6bc093a1012c2f9c0cfeadfb4b41b554c51ca4f0
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-04-04 14:48:31 +0200

    Move Sgallery to browsers

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -384,6 +384,7 @@ class Browser extends AbstractClientParser
         'SD' => 'SavySoda',
         'S9' => 'Secure Browser',
         'SV' => 'SFive',
+        'SJ' => 'Sgallery',
         'SH' => 'Shiira',
         'S1' => 'SimpleBrowser',
         '3S' => 'SilverMob US',
@@ -493,7 +494,7 @@ class Browser extends AbstractClientParser
             'HP', 'IO', 'TP', 'CJ', 'HQ', 'HI', 'NA', 'BW', 'YO',
             'DC', 'G8', 'DT', 'AP', 'AK', 'UI', 'SD', 'VN', '4S',
             '2S', 'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0',
-            'C8', 'AZ', 'MM', 'BT', 'N0', 'P0', 'C9', '01',
+            'C8', 'AZ', 'MM', 'BT', 'N0', 'P0', 'C9', '01', 'SJ',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -527,7 +528,7 @@ class Browser extends AbstractClientParser
         'WP', 'YN', 'IO', 'IS', 'HQ', 'RW', 'HI', 'NA', 'BW',
         'YO', 'PK', 'MR', 'AP', 'AK', 'UI', 'SD', 'VN', '4S',
         'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0', 'C8',
-        'AZ', 'MM', 'BT', 'N0', 'P0', 'C9', '01',
+        'AZ', 'MM', 'BT', 'N0', 'P0', 'C9', '01', 'SJ',
     ];
 
     /**
