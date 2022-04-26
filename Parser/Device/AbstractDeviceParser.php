3d57c2c66017f657361de587a3fdacf7e97d440f
sanchezzzhak <sanchezzzhak@ya.ru>
2022-04-26 19:58:07 +0300

    feat(device) detect new brand Zonko: K101 feat(device) detect brand Proline: H10882M feat(device) detect brand VKworld: VK7000 feat(device) detect brand Blu: Grand Energy, Studio G HD LTE feat(device) detect brand Zopo: Hero 2 feat(device) detect brand Highscreen: Thunder

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -1345,6 +1345,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         'FZ' => 'ZIFRO',
         'ZX' => 'Ziox',
         'ZO' => 'Zonda',
+        'ZW' => 'Zonko',
         'ZP' => 'Zopo',
         'ZT' => 'ZTE',
         'ZU' => 'Zuum',
