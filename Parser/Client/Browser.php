eae5d7fff156cadbf614d46ac7411453dbcbfed5
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-08-22 20:57:38 +0200

    Adds detection for Black Lion Browser
    ref #7039

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -118,6 +118,7 @@ class Browser extends AbstractClientParser
         'B0' => 'Bloket',
         'BJ' => 'Bunjalloo',
         'BL' => 'B-Line',
+        'B6' => 'Black Lion Browser',
         'BU' => 'Blue Browser',
         'BO' => 'Bonsai',
         'BN' => 'Borealis Navigator',
@@ -552,7 +553,7 @@ class Browser extends AbstractClientParser
             'C8', 'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'VS', 'DU',
             'D0', 'P1', 'O4', '8S', 'H3', 'TE', 'WB', 'K1', 'P2',
             'XO', 'U0', 'B0', 'VA', 'X0', 'NX', 'O5', 'R1', 'I1',
-            'HO', 'A5', 'I8', '18', 'B5',
+            'HO', 'A5', 'I8', '18', 'B5', 'B6',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -588,7 +589,7 @@ class Browser extends AbstractClientParser
         'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0', 'C8',
         'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'DU', 'D0', 'P1',
         'O4', 'XO', 'U0', 'B0', 'VA', 'X0', 'A5', 'I8', '18',
-        'B5',
+        'B5', 'B6',
     ];
 
     /**
