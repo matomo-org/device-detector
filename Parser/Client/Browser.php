c57c4b3de027b3fa1a0f142d172ccfee437c55bc
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-08-23 16:15:52 +0200

    Adds detection for Fiery Browser
    ref #7039

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -202,6 +202,7 @@ class Browser extends AbstractClientParser
         'ES' => 'Espial TV Browser',
         'FA' => 'Falkon',
         'FX' => 'Faux Browser',
+        'F4' => 'Fiery Browser',
         'F1' => 'Firefox Mobile iOS',
         'FB' => 'Firebird',
         'FD' => 'Fluid',
@@ -557,6 +558,7 @@ class Browser extends AbstractClientParser
             'D0', 'P1', 'O4', '8S', 'H3', 'TE', 'WB', 'K1', 'P2',
             'XO', 'U0', 'B0', 'VA', 'X0', 'NX', 'O5', 'R1', 'I1',
             'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC', 'A6', '2X',
+            'F4',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -592,7 +594,7 @@ class Browser extends AbstractClientParser
         'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0', 'C8',
         'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'DU', 'D0', 'P1',
         'O4', 'XO', 'U0', 'B0', 'VA', 'X0', 'A5', 'X1', '18',
-        'B5', 'B6', 'TC', 'A6', '2X',
+        'B5', 'B6', 'TC', 'A6', '2X', 'F4',
     ];
 
     /**
