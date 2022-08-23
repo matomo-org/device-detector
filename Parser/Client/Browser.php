1c1f543d6e30323cd40f0bc428479121bc253312
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-08-23 16:55:28 +0200

    Adds detection for NextWord Browser
    ref #7039

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -339,6 +339,7 @@ class Browser extends AbstractClientParser
         'NL' => 'NetFront Life',
         'NP' => 'NetPositive',
         'NS' => 'Netscape',
+        'WR' => 'NextWord Browser',
         'NT' => 'NTENT Browser',
         'OC' => 'Oculus Browser',
         'O1' => 'Opera Mini iOS',
@@ -559,7 +560,7 @@ class Browser extends AbstractClientParser
             'D0', 'P1', 'O4', '8S', 'H3', 'TE', 'WB', 'K1', 'P2',
             'XO', 'U0', 'B0', 'VA', 'X0', 'NX', 'O5', 'R1', 'I1',
             'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC', 'A6', '2X',
-            'F4', 'YG',
+            'F4', 'YG', 'WR',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -595,7 +596,7 @@ class Browser extends AbstractClientParser
         'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0', 'C8',
         'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'DU', 'D0', 'P1',
         'O4', 'XO', 'U0', 'B0', 'VA', 'X0', 'A5', 'X1', '18',
-        'B5', 'B6', 'TC', 'A6', '2X', 'F4', 'YG',
+        'B5', 'B6', 'TC', 'A6', '2X', 'F4', 'YG', 'WR',
     ];
 
     /**
