1a45963676666ec1a6817f3639b1bf2559415b9d
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-08-23 15:06:10 +0200

    Adds detection for TUC Mini Browser
    ref #7039

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -468,6 +468,7 @@ class Browser extends AbstractClientParser
         'TE' => 'Tesla Browser',
         'TZ' => 'Tizen Browser',
         'TI' => 'Tint Browser',
+        'TC' => 'TUC Mini Browser',
         'TU' => 'Tungsten',
         'TG' => 'ToGate',
         'TS' => 'TweakStyle',
@@ -553,7 +554,7 @@ class Browser extends AbstractClientParser
             'C8', 'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'VS', 'DU',
             'D0', 'P1', 'O4', '8S', 'H3', 'TE', 'WB', 'K1', 'P2',
             'XO', 'U0', 'B0', 'VA', 'X0', 'NX', 'O5', 'R1', 'I1',
-            'HO', 'A5', 'X1', '18', 'B5', 'B6',
+            'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -589,7 +590,7 @@ class Browser extends AbstractClientParser
         'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0', 'C8',
         'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'DU', 'D0', 'P1',
         'O4', 'XO', 'U0', 'B0', 'VA', 'X0', 'A5', 'X1', '18',
-        'B5', 'B6',
+        'B5', 'B6', 'TC',
     ];
 
     /**
