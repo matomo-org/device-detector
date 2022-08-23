67c6e8a8f8b458174cc32d480f243e7ec2b53af8
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-08-23 17:02:48 +0200

    Adds detection for Naked Browser Pro
    ref #7039

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -329,6 +329,7 @@ class Browser extends AbstractClientParser
         'MW' => 'MAUI WAP Browser',
         'NW' => 'Navigateur Web',
         'NK' => 'Naked Browser',
+        'NA' => 'Naked Browser Pro',
         'NR' => 'NFS Browser',
         'NB' => 'Nokia Browser',
         'NO' => 'Nokia OSS Browser',
@@ -560,7 +561,7 @@ class Browser extends AbstractClientParser
             'D0', 'P1', 'O4', '8S', 'H3', 'TE', 'WB', 'K1', 'P2',
             'XO', 'U0', 'B0', 'VA', 'X0', 'NX', 'O5', 'R1', 'I1',
             'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC', 'A6', '2X',
-            'F4', 'YG', 'WR',
+            'F4', 'YG', 'WR', 'NA',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -596,7 +597,7 @@ class Browser extends AbstractClientParser
         'RF', 'LR', 'SQ', 'BV', 'L1', 'F0', 'KS', 'V0', 'C8',
         'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'DU', 'D0', 'P1',
         'O4', 'XO', 'U0', 'B0', 'VA', 'X0', 'A5', 'X1', '18',
-        'B5', 'B6', 'TC', 'A6', '2X', 'F4', 'YG', 'WR',
+        'B5', 'B6', 'TC', 'A6', '2X', 'F4', 'YG', 'WR', 'NA',
     ];
 
     /**
