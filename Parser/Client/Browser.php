e414044bb50e6bea7cc3d137648613a7790be508
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2022-08-23 19:45:04 +0200

    Adds detection for 1DM Browser
    ref #7039

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -68,6 +68,7 @@ class Browser extends AbstractClientParser
         'DW' => 'Dark Web Browser',
         '18' => '18+ Privacy Browser',
         '1B' => '115 Browser',
+        'DM' => '1DM Browser',
         '2B' => '2345 Browser',
         '3B' => '360 Browser',
         '36' => '360 Phone Browser',
@@ -562,7 +563,7 @@ class Browser extends AbstractClientParser
             'D0', 'P1', 'O4', '8S', 'H3', 'TE', 'WB', 'K1', 'P2',
             'XO', 'U0', 'B0', 'VA', 'X0', 'NX', 'O5', 'R1', 'I1',
             'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC', 'A6', '2X',
-            'F4', 'YG', 'WR', 'NA',
+            'F4', 'YG', 'WR', 'NA', 'DM',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -599,6 +600,7 @@ class Browser extends AbstractClientParser
         'AZ', 'MM', 'BT', 'N0', 'P0', 'F3', 'DU', 'D0', 'P1',
         'O4', 'XO', 'U0', 'B0', 'VA', 'X0', 'A5', 'X1', '18',
         'B5', 'B6', 'TC', 'A6', '2X', 'F4', 'YG', 'WR', 'NA',
+        'DM',
     ];
 
     /**
