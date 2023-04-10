50fb82248c3ffd87b522f15e040f796c354bbc7a
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2023-04-10 12:53:10 +0200

    Adds detection for Wavebox browser (#7385)
    * Adds detection for Wavebox
    * Adds client hints test for Microsoft Edge
    * Adds client hints test for Chrome

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -518,6 +518,7 @@ class Browser extends AbstractClientParser
         'WE' => 'WebPositive',
         'WF' => 'Waterfox',
         'WB' => 'Wave Browser',
+        'WA' => 'Wavebox',
         'WH' => 'Whale Browser',
         'WO' => 'wOSBrowser',
         'WT' => 'WeTab Browser',
@@ -582,7 +583,7 @@ class Browser extends AbstractClientParser
             'XO', 'U0', 'B0', 'VA', 'X0', 'NX', 'O5', 'R1', 'I1',
             'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC', 'A6', '2X',
             'F4', 'YG', 'WR', 'NA', 'DM', '1M', 'A7', 'XN', 'XT',
-            'XB', 'W1', 'HT', 'B8', 'F5', 'B9',
+            'XB', 'W1', 'HT', 'B8', 'F5', 'B9', 'WA',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
