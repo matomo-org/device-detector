38e3bb22fa46a6daca15a38d05964c4b43209a8e
sanchezzzhak <sanchezzzhak@ya.ru>
2023-07-03 17:39:55 +0300

    fix: typo

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -1866,7 +1866,7 @@ abstract class AbstractDeviceParser extends AbstractParser
             $osVersion = $this->clientHints->getOperatingSystemVersion();
             $this->setUserAgent(\preg_replace(
                 '(Android 10[.\d]*; K)',
-                \sprintf('Android %s; %s', '' !== $osVersion ?: '10', $deviceModel),
+                \sprintf('Android %s; %s', '' !== $osVersion ? $osVersion : '10', $deviceModel),
                 $this->userAgent
             ));
         }
