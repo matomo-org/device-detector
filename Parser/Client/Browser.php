8eb09db657454dd8b59c44885ecdc791cacb4c81
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2023-09-05 03:04:13 +0200

    Adds detection for Norton Secure Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -349,6 +349,7 @@ class Browser extends AbstractClientParser
         'NB' => 'Nokia Browser',
         'NO' => 'Nokia OSS Browser',
         'NV' => 'Nokia Ovi Browser',
+        'N2' => 'Norton Secure Browser',
         'NX' => 'Nox Browser',
         'N1' => 'NOMone VR Browser',
         'NE' => 'NetSurf',
@@ -596,7 +597,7 @@ class Browser extends AbstractClientParser
             'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC', 'A6', '2X',
             'F4', 'YG', 'WR', 'NA', 'DM', '1M', 'A7', 'XN', 'XT',
             'XB', 'W1', 'HT', 'B8', 'F5', 'B9', 'WA', 'T0', 'HC',
-            'O6', 'P7', 'LJ', 'LC', 'O7',
+            'O6', 'P7', 'LJ', 'LC', 'O7', 'N2',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
