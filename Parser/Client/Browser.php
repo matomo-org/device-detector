23e3042e34b08ccffbb8c892bb4a2a970ca55f16
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2023-11-13 14:30:24 +0100

    Fix

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -599,7 +599,7 @@ class Browser extends AbstractClientParser
             'HO', 'A5', 'X1', '18', 'B5', 'B6', 'TC', 'A6', '2X',
             'F4', 'YG', 'WR', 'NA', 'DM', '1M', 'A7', 'XN', 'XT',
             'XB', 'W1', 'HT', 'B8', 'F5', 'B9', 'WA', 'T0', 'HC',
-            'O6', 'P7', 'LJ', 'LC', 'O7', 'N2', 'A8', 'P8', 'H1',
+            'O6', 'P7', 'LJ', 'LC', 'O7', 'N2', 'A8', 'P8',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
