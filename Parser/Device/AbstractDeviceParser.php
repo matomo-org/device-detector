e88a364463d0c22fbd0f35a492159e4c5e2eeb59
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2023-12-20 11:24:35 +0100

    Adds detection for Sagemcom brand
    test user agent

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -1363,6 +1363,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         'SLF' => 'SAILF',
         'X1'  => 'Safaricom',
         'SG'  => 'Sagem',
+        'SAG' => 'Sagemcom',
         '4L'  => 'Salora',
         'SA'  => 'Samsung',
         'SAT' => 'Samtech',
