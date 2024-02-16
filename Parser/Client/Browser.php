25d7b204fef5609602d18393c6871948615ba1b8
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-02-16 11:06:33 +0100

    Adds detection for SberBrowser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -469,6 +469,7 @@ class Browser extends AbstractClientParser
         'SB' => 'Samsung Browser',
         '3L' => 'Samsung Browser Lite',
         'SA' => 'Sailfish Browser',
+        'R0' => 'SberBrowser',
         'S8' => 'Seewo Browser',
         'SC' => 'SEMC-Browser',
         'SE' => 'Sogou Explorer',
@@ -642,7 +643,7 @@ class Browser extends AbstractClientParser
             '1W', 'EV', 'I9', 'V4', 'H4', '1T', 'M5', '0S', '0C',
             'ZR', 'D6', 'F6', 'RC', 'WD', 'P3', 'FT', 'A9', 'X2',
             'N3', 'GD', 'O9', 'Q3', 'F7', 'K2', 'P5', 'H5', 'V3',
-            'K3', 'Q4', 'G2',
+            'K3', 'Q4', 'G2', 'R0',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
