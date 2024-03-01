a6163559ca707c6b478812d9d0b9afe67b982c50
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-01 21:38:13 +0100

    Adds detection for TTWebview

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -537,6 +537,7 @@ class Browser extends AbstractClientParser
         'TE' => 'Tesla Browser',
         'TZ' => 'Tizen Browser',
         'TI' => 'Tint Browser',
+        'TT' => 'TTWebview',
         'TC' => 'TUC Mini Browser',
         'TU' => 'Tungsten',
         'TG' => 'ToGate',
@@ -643,7 +644,7 @@ class Browser extends AbstractClientParser
             '1W', 'EV', 'I9', 'V4', 'H4', '1T', 'M5', '0S', '0C',
             'ZR', 'D6', 'F6', 'RC', 'WD', 'P3', 'FT', 'A9', 'X2',
             'N3', 'GD', 'O9', 'Q3', 'F7', 'K2', 'P5', 'H5', 'V3',
-            'K3', 'Q4', 'G2', 'R2',
+            'K3', 'Q4', 'G2', 'R2', 'TT',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
