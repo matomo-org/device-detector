c1cb50d6ec530919e9c1827b2d3d06c90ceacccb
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-02 18:20:58 +0100

    Adds detection for Intune Managed Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -298,6 +298,7 @@ class Browser extends AbstractClientParser
         'I9' => 'Insta Browser',
         'IE' => 'Internet Explorer',
         'I7' => 'Internet Browser Secure',
+        '3I' => 'Intune Managed Browser',
         'I5' => 'Indian UC Mini Browser',
         'Z0' => 'InBrowser',
         'IM' => 'IE Mobile',
@@ -658,7 +659,7 @@ class Browser extends AbstractClientParser
             '1W', 'EV', 'I9', 'V4', 'H4', '1T', 'M5', '0S', '0C',
             'ZR', 'D6', 'F6', 'RC', 'WD', 'P3', 'FT', 'A9', 'X2',
             'N3', 'GD', 'O9', 'Q3', 'F7', 'K2', 'P5', 'H5', 'V3',
-            'K3', 'Q4', 'G2', 'R2', 'TT', 'WX', 'XP',
+            'K3', 'Q4', 'G2', 'R2', 'TT', 'WX', 'XP', '3I',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
