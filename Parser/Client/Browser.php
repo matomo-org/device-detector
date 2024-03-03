45b4e4578ae64c5e98bbe64b48a96f55f6154124
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-03 18:45:13 +0100

    Change short code for Moya Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -363,7 +363,7 @@ class Browser extends AbstractClientParser
         'MO' => 'Mobicip',
         'MU' => 'MIUI Browser',
         'MS' => 'Mobile Silk',
-        'M6' => 'Moya Browser',
+        'M8' => 'Moya Browser',
         'MN' => 'Minimo',
         'MT' => 'Mint Browser',
         'MX' => 'Maxthon',
@@ -705,7 +705,7 @@ class Browser extends AbstractClientParser
         '1W', 'EV', 'Z0', 'I9', 'V4', 'H4', 'M5', '0S', '0C',
         'ZR', 'D6', 'F6', 'P3', 'FT', 'A9', 'X2', 'NI', 'FG',
         'TH', 'N3', 'GD', 'O9', 'Q3', 'F7', 'K2', 'N4', 'P5',
-        'H5', 'V3', 'G2', 'M6', 'BG',
+        'H5', 'V3', 'G2', 'M8', 'BG',
     ];
 
     /**
