8abcbf2d137fee9328bd8bfc0bd90b011b1269ca
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-06 19:22:07 +0100

    Remove TTWebview

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -558,7 +558,6 @@ class Browser extends AbstractClientParser
         'TZ' => 'Tizen Browser',
         'TI' => 'Tint Browser',
         'TL' => 'TrueLocation Browser',
-        'TT' => 'TTWebview',
         'TC' => 'TUC Mini Browser',
         'TU' => 'Tungsten',
         'TG' => 'ToGate',
@@ -667,8 +666,8 @@ class Browser extends AbstractClientParser
             '1W', 'EV', 'I9', 'V4', 'H4', '1T', 'M5', '0S', '0C',
             'ZR', 'D6', 'F6', 'RC', 'WD', 'P3', 'FT', 'A9', 'X2',
             'N3', 'GD', 'O9', 'Q3', 'F7', 'K2', 'P5', 'H5', 'V3',
-            'K3', 'Q4', 'G2', 'R2', 'TT', 'WX', 'XP', '3I', 'BG',
-            'R0', 'JO', 'OL',
+            'K3', 'Q4', 'G2', 'R2', 'WX', 'XP', '3I', 'BG', 'R0',
+            'JO', 'OL',
         ],
         'Firefox'            => [
             'AX', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
