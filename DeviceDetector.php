be8de2ce04457e2024cbc15e47a3b074636b05c5
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-07 21:12:43 +0100

    Adds detection for various browsers (#7614)
    * Adds detection for TTWebview
    
    * Adds detection for tararia
    
    * Adds detection for Colom Browser
    
    * Adds detection for VibeMate
    
    * Improves detection for VibeMate
    
    * Improves detection for Chrome Webview
    
    * Adds detection for Rakuten Browser
    
    * Adds detection for Rakuten Web Search
    
    * Fix VibeMate
    
    * Remove null from family
    
    * Fix Chrome Webview tests
    
    * Fix Rakuten Web Search test
    
    * Adds detection for Moya Browser
    
    * Adds detection for Impervious Browser
    
    * Improves version detection for Nokia Browser
    
    * Improves version detection for VibeMate
    
    * Adds detection for Catalyst
    
    * Adds detection for Wexond
    
    * Adds detection for Ninetails
    
    * Adds detection for JUZI Browser
    
    * Adds detection for Lotus
    
    * Adds detection for Catsxp
    
    https://www.catsxp.com/
    
    * Adds detection for QJY TV Browser
    
    * Improves detection for VMware AirWatch
    
    * Adds detection for Intune Managed Browser
    
    * Adds detection for Bang
    
    * Fix QJY TV Browser name
    
    * Improves detection for Ask.com
    
    * Adds tests for Ghostery Privacy Browser
    
    * Adds detection for GoKu
    
    * Delete .phpunit.result.cache
    
    * Adds detection for Cliqz
    
    Replaced by Ghostery Privacy Browser
    
    * Improves detection for 18+ Privacy Browser
    
    * Adds detection for Surfy Browser
    
    * Improves detection for Surfy Browser
    
    * Improves detection for Vision Mobile Browser
    
    * Improves detection for GinxDroid Browser
    
    * Change short code for Moya Browser
    
    * Move F-Secure SAFE from apps to browsers
    
    * Move KPN Veilig from apps to browsers
    
    * Adds detection for Bluefy
    
    * Improve regexes
    
    * Sort keys
    
    * Add Microsoft Edge to $clientHintMapping
    
    * Improves detection for Edge WebView
    
    * Adds test for Chrome Webview
    
    * Remove information not required for browser tests
    
    * Improves detection for 360 Secure Browser
    
    * Improves detection for Vivaldi
    
    * Improves version detection for Brave
    
    * Adds detection for OnBrowser Lite
    
    * Adds detection for Diigo Browser
    
    * Adds detection for TrueLocation Browser
    
    * Adds detection for Mogok Browser
    
    ---------
    
    Co-authored-by: Tutik Alexsandr <sanchezzzhak@ya.ru>

--- DeviceDetector.php
+++ DeviceDetector.php
@@ -1065,7 +1065,7 @@ class DeviceDetector
          */
         if (\in_array($clientName, [
             'Kylo', 'Espial TV Browser', 'LUJO TV Browser', 'LogicUI TV Browser', 'Open TV Browser', 'Seraphic Sraf',
-            'Opera Devices', 'Crow Browser', 'Vewd Browser', 'TiviMate', 'Quick Search TV',
+            'Opera Devices', 'Crow Browser', 'Vewd Browser', 'TiviMate', 'Quick Search TV', 'QJY TV Browser',
         ])
         ) {
             $this->device = AbstractDeviceParser::DEVICE_TYPE_TV;
