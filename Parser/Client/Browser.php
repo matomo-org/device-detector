f29f109f9c0244214fd33ab1404cf47b8a407f37
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-10 11:56:35 +0100

    Rename AOL to AOL Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -93,7 +93,7 @@ class Browser extends AbstractClientParser
         'A5' => 'Amerigo',
         'AO' => 'Amigo',
         'AN' => 'Android Browser',
-        '3A' => 'AOL',
+        '3A' => 'AOL Browser',
         'AE' => 'AOL Desktop',
         'AD' => 'AOL Shield',
         'A4' => 'AOL Shield Pro',
