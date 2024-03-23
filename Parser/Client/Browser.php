5cbf3908ec3c04eadd8a38daac4305fdf63b098f
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-23 15:11:32 +0100

    Adds detection for Proxynet

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -482,6 +482,7 @@ class Browser extends AbstractClientParser
         'P4' => 'Privacy Explorer Fast Safe',
         'P3' => 'Private Internet Browser',
         'P5' => 'Proxy Browser',
+        '6P' => 'Proxynet',
         'P2' => 'Pi Browser',
         'P0' => 'PronHub Browser',
         'PC' => 'PSI Secure Browser',
@@ -745,7 +746,7 @@ class Browser extends AbstractClientParser
         'TH', 'N3', 'GD', 'O9', 'Q3', 'F7', 'K2', 'N4', 'P5',
         'H5', 'V3', 'G2', 'BG', 'OL', 'II', 'TL', 'M6', 'Y3',
         'M7', 'GN', 'D3', 'IG', 'HW', '4O', 'OU', '5P', 'KE',
-        '5A', 'TT',
+        '5A', 'TT', '6P',
     ];
 
     /**
