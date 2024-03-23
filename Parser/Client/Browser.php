253bf872fc08a57b8ffb99ba3d58b73c98f35a7d
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-03-23 15:22:10 +0100

    Adds detection for BBQ Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -129,6 +129,7 @@ class Browser extends AbstractClientParser
         'BG' => 'Bang',
         'B9' => 'Bangla Browser',
         'BI' => 'Basilisk',
+        'BQ' => 'BBQ Browser',
         'BV' => 'Belva Browser',
         'B5' => 'Beyond Private Browser',
         'BE' => 'Beonex',
@@ -749,7 +750,7 @@ class Browser extends AbstractClientParser
         'TH', 'N3', 'GD', 'O9', 'Q3', 'F7', 'K2', 'N4', 'P5',
         'H5', 'V3', 'G2', 'BG', 'OL', 'II', 'TL', 'M6', 'Y3',
         'M7', 'GN', 'D3', 'IG', 'HW', '4O', 'OU', '5P', 'KE',
-        '5A', 'TT', '6P', 'G3', '7P', 'VU',
+        '5A', 'TT', '6P', 'G3', '7P', 'VU', 'BQ',
     ];
 
     /**
