3f9112cc823f38224e4ea91362c70e3b77226419
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-08-26 12:25:02 +0200

    Revert "Fix Yoda comparison"
    This reverts commit 42f42af3fbeda9b6fb7d75b6c99f04a01f24b9e3.

--- Parser/Client/AbstractClientParser.php
+++ Parser/Client/AbstractClientParser.php
@@ -85,7 +85,7 @@ abstract class AbstractClientParser extends AbstractParser
             $names[] = $regex['name'];
         }
 
-        if (MobileApp::class === static::class) {
+        if (static::class === MobileApp::class) {
             $names = \array_merge($names, [
                 // Microsoft Office $1
                 'Microsoft Office Access', 'Microsoft Office Excel', 'Microsoft Office OneDrive for Business',
