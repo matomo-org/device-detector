43205746819f40a6fed93b6f6704b39f363ef7c1
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2024-09-10 20:46:59 +0200

    Replace Puffin Secure Browser with Puffin Web Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -793,7 +793,7 @@ class Browser extends AbstractClientParser
         '36', 'AH', 'AI', 'BL', 'C1', 'C4', 'CB', 'CW', 'DB',
         '3M', 'DT', 'EU', 'EZ', 'FK', 'FM', 'FR', 'FX', 'GH',
         'GI', 'GR', 'HA', 'HU', 'IV', 'JB', 'KD', 'M1', 'MF',
-        'MN', 'MZ', 'NX', 'OC', 'OI', 'OM', 'OZ', 'PU', 'PI',
+        'MN', 'MZ', 'NX', 'OC', 'OI', 'OM', 'OZ', '2P', 'PI',
         'PE', 'QU', 'RE', 'S0', 'S7', 'SA', 'SB', 'SG', 'SK',
         'ST', 'SU', 'T1', 'UH', 'UM', 'UT', 'VE', 'VV', 'WI',
         'WP', 'YN', 'IO', 'IS', 'HQ', 'RW', 'HI', 'PN', 'BW',
