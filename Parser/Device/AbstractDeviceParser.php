dfbc01b79f72ba34f3e5bed51d0dd3768c6849d1
Dmitrii Kustov <github@simbiat.dev>
2025-09-10 19:33:55 +0300

    Unonu to Unonu Mobile

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -1921,7 +1921,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         'UO'  => 'Unnecto',
         'UNN' => 'Unnion Technologies',
         'UNP' => 'UnoPhone',
-        'UU'  => 'Unonu',
+        'UU'  => 'Unonu Mobile',
         'UN'  => 'Unowhy',
         'UY'  => 'UNNO',
         'UOO' => 'UOOGOU',
