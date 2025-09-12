75132dbf325d0dbb0ed3ba10fd46158b6e499975
Dmitrii Kustov <github@simbiat.dev>
2025-09-12 20:32:47 +0300

    No devices seem to resolve to "Web TV" brand

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -2173,7 +2173,6 @@ abstract class AbstractDeviceParser extends AbstractParser
         'WBF' => 'Webfleet',
         'WSS' => 'WS',
         // legacy brands, might be removed in future versions
-        'WB'  => 'Web TV',
         'XX'  => 'Unknown',
     ];
 
