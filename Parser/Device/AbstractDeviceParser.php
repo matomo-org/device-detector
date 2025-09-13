de05d54d907dea37045e29fa1b3f448f555c4217
Dmitrii Kustov <github@simbiat.dev>
2025-09-13 20:09:55 +0300

    ClearPHONE to Clear

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -415,7 +415,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         '75'  => 'Covia',
         'QG'  => 'COYOTE',
         'CKK' => 'CKK Mobile',
-        'YW'  => 'ClearPHONE',
+        'YW'  => 'Clear',
         '33'  => 'Clementoni',
         'CR'  => 'CreNova',
         'CX'  => 'Crescent',
