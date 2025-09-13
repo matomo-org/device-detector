c62cb1f4067fb57e7d8184086d9f07940ecb81c6
Dmitrii Kustov <github@simbiat.dev>
2025-09-13 19:21:58 +0300

    iNavo to iNavi Systems

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -936,7 +936,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         '32'  => 'Invens Mobile',
         '4I'  => 'Invin',
         'IFT' => 'iFIT',
-        'INA' => 'iNavi',
+        'INA' => 'iNavi Systems',
         'I1'  => 'iOcean',
         'IMU' => 'iMuz',
         'IP'  => 'iPro',
