6db928a245f23619264f9eb4dd91825d121ba26d
Dmitrii Kustov <github@simbiat.dev>
2025-09-13 09:52:51 +0300

    White Mobile to Cherry Mobile

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -2037,7 +2037,6 @@ abstract class AbstractDeviceParser extends AbstractParser
         'WAL' => 'Waltter',
         'WAI' => 'Wainyok',
         'WAT' => 'waipu.tv',
-        'WHI' => 'White Mobile',
         'WHO' => 'Whoop',
         'WBL' => 'We. by Loewe.',
         'WCP' => 'WeChip',
