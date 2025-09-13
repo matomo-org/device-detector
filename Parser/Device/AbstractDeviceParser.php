87cd05108dadef74376809bd0f261ba80398c03d
Dmitrii Kustov <github@simbiat.dev>
2025-09-13 19:26:35 +0300

    Huskee to Huskee Mobile

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -862,7 +862,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         'HUM' => 'Humanware',
         'HME' => 'HUMElab',
         'HR'  => 'Hurricane',
-        'H5'  => 'Huskee',
+        'H5'  => 'Huskee Mobile',
         'HUG' => 'Hugerock',
         'HY'  => 'Hyrican',
         'HN'  => 'Hyundai',
