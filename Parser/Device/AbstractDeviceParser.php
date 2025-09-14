28d8b916f410cc26e9e358fd77ac9dd358614b17
Dmitrii Kustov <github@simbiat.dev>
2025-09-14 10:16:52 +0300

    EFT to EFT Group

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -549,7 +549,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         'E6'  => 'EE',
         'GW'  => 'EGL',
         'EGO' => 'EGOTEK',
-        'EFT' => 'EFT',
+        'EFT' => 'EFT Group',
         'EK'  => 'EKO',
         'EY'  => 'Einstein',
         'EM'  => 'Eks Mobility',
