56baf981af4f192e15a4f369d4975af847a81ccb
Stefan Giehl <stefan@matomo.org>
2025-11-26 17:02:47 +0100

    Prepare next release (#8155)

--- DeviceDetector.php
+++ DeviceDetector.php
@@ -68,7 +68,7 @@ class DeviceDetector
     /**
      * Current version number of DeviceDetector
      */
-    public const VERSION = '6.4.7';
+    public const VERSION = '6.4.8';
 
     /**
      * Constant used as value for unknown browser / os
