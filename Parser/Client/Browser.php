7395b80bade78b3c51ed552ed304f480755a1fc2
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-11-29 09:04:33 +0100

    Adds detection for Ray browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -559,6 +559,7 @@ class Browser extends AbstractClientParser
         'R3' => 'Rakuten Browser',
         'R4' => 'Rakuten Web Search',
         'R2' => 'Raspbian Chromium',
+        'R6' => 'Ray',
         'RT' => 'RCA Tor Explorer',
         'RE' => 'Realme Browser',
         'RK' => 'Rekonq',
@@ -780,7 +781,7 @@ class Browser extends AbstractClientParser
             'M9', 'F9', '0P', '0A', 'JR', 'D3', 'TK', 'BP', '2F',
             '2M', 'K7', '1N', '8A', 'H7', 'X3', 'T4', 'X4', '5O',
             '8C', '3M', '6I', '2P', 'PU', '7I', 'X5', 'AL', '3P',
-            'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00',
+            'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00', 'R6',
         ],
         'Firefox'            => [
             'FF', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
