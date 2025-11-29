85c4fb75b25d1e230a71628d9df99b7553d8ea8f
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-11-29 09:06:58 +0100

    Adds detection for Doubao browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -231,6 +231,7 @@ class Browser extends AbstractClientParser
         'DO' => 'Dorado',
         'DR' => 'Dot Browser',
         'DL' => 'Dooble',
+        'D8' => 'Doubao',
         'DI' => 'Dillo',
         'DU' => 'DUC Browser',
         'DD' => 'DuckDuckGo Privacy Browser',
@@ -781,7 +782,7 @@ class Browser extends AbstractClientParser
             'M9', 'F9', '0P', '0A', 'JR', 'D3', 'TK', 'BP', '2F',
             '2M', 'K7', '1N', '8A', 'H7', 'X3', 'T4', 'X4', '5O',
             '8C', '3M', '6I', '2P', 'PU', '7I', 'X5', 'AL', '3P',
-            'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00', 'R6',
+            'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00', 'R6', 'D8',
         ],
         'Firefox'            => [
             'FF', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
