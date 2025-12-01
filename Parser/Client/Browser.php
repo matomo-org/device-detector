948be5004f508b86f4095917eaa485743813557d
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-12-01 06:00:33 +0100

    Adds detection for Nova Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -685,6 +685,7 @@ class Browser extends AbstractClientParser
         'VE' => 'Venus Browser',
         'WD' => 'Vewd Browser',
         'V5' => 'VibeMate',
+        'NQ' => 'Nova Browser',
         'N0' => 'Nova Video Downloader Pro',
         'VS' => 'Viasat Browser',
         'VI' => 'Vivaldi',
@@ -796,7 +797,7 @@ class Browser extends AbstractClientParser
             '8C', '3M', '6I', '2P', 'PU', '7I', 'X5', 'AL', '3P',
             'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00', 'R6', 'D8',
             'PQ', 'LM', 'T5', '2N', 'SJ', 'X6', 'SM', 'AY', 'BQ',
-            'BC',
+            'BC', 'NQ',
         ],
         'Firefox'            => [
             'FF', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -849,7 +850,7 @@ class Browser extends AbstractClientParser
         '9P', 'N8', 'VR', 'N9', 'M9', 'F9', '0P', '0A', '2F',
         '2M', 'K7', '1N', '8A', 'H7', 'X3', 'X4', '5O', '6I',
         '7I', 'X5', '3P', '2E', 'T5', '2N', 'SJ', 'X6', 'SM',
-        'AY', 'BQ', 'BC',
+        'AY', 'BQ', 'BC', 'NQ',
     ];
 
     /**
