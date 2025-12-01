9ab12b84cbe9bf6b6e7526bfa1fc6ea45fc7d97e
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-12-01 06:44:52 +0100

    Adds detection for DDU Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -221,6 +221,7 @@ class Browser extends AbstractClientParser
         'PD' => 'Peeps dBrowser',
         'DK' => 'Dark Web',
         'DP' => 'Dark Web Private',
+        'D4' => 'DDU Browser',
         'D1' => 'Debuggable Browser',
         'DC' => 'Decentr',
         'DE' => 'Deepnet Explorer',
@@ -801,7 +802,7 @@ class Browser extends AbstractClientParser
             '8C', '3M', '6I', '2P', 'PU', '7I', 'X5', 'AL', '3P',
             'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00', 'R6', 'D8',
             'PQ', 'LM', 'T5', '2N', 'SJ', 'X6', 'SM', 'AY', 'BQ',
-            'BC', 'NQ', 'VQ', '9C', 'KA', 'YS',
+            'BC', 'NQ', 'VQ', '9C', 'KA', 'YS', 'D4',
         ],
         'Firefox'            => [
             'FF', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -854,7 +855,7 @@ class Browser extends AbstractClientParser
         '9P', 'N8', 'VR', 'N9', 'M9', 'F9', '0P', '0A', '2F',
         '2M', 'K7', '1N', '8A', 'H7', 'X3', 'X4', '5O', '6I',
         '7I', 'X5', '3P', '2E', 'T5', '2N', 'SJ', 'X6', 'SM',
-        'AY', 'BQ', 'BC', 'NQ', 'VQ', 'KA', 'YS',
+        'AY', 'BQ', 'BC', 'NQ', 'VQ', 'KA', 'YS', 'D4',
     ];
 
     /**
