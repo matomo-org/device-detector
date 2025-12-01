46dd12fe1d0aa4b10f605c8288ff88f73193f32a
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-12-01 06:41:04 +0100

    Adds detection for Stay Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -637,6 +637,7 @@ class Browser extends AbstractClientParser
         'RY' => 'Surfy Browser',
         'SG' => 'Stargon',
         'S0' => 'START Internet Browser',
+        'YS' => 'Stay Browser',
         '5A' => 'Stealth Browser',
         'S4' => 'Steam In-Game Overlay',
         'ST' => 'Streamy',
@@ -800,7 +801,7 @@ class Browser extends AbstractClientParser
             '8C', '3M', '6I', '2P', 'PU', '7I', 'X5', 'AL', '3P',
             'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00', 'R6', 'D8',
             'PQ', 'LM', 'T5', '2N', 'SJ', 'X6', 'SM', 'AY', 'BQ',
-            'BC', 'NQ', 'VQ', '9C', 'KA',
+            'BC', 'NQ', 'VQ', '9C', 'KA', 'YS',
         ],
         'Firefox'            => [
             'FF', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -853,7 +854,7 @@ class Browser extends AbstractClientParser
         '9P', 'N8', 'VR', 'N9', 'M9', 'F9', '0P', '0A', '2F',
         '2M', 'K7', '1N', '8A', 'H7', 'X3', 'X4', '5O', '6I',
         '7I', 'X5', '3P', '2E', 'T5', '2N', 'SJ', 'X6', 'SM',
-        'AY', 'BQ', 'BC', 'NQ', 'VQ', 'KA',
+        'AY', 'BQ', 'BC', 'NQ', 'VQ', 'KA', 'YS',
     ];
 
     /**
