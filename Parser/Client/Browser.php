660c5b5be73700f2fe5c31116053acd337b92b28
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-12-01 05:37:50 +0100

    Adds detection for Snap Browser

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -615,6 +615,7 @@ class Browser extends AbstractClientParser
         '6S' => 'Smart Search & Web Browser',
         'LE' => 'Smart Lenovo Browser',
         'OZ' => 'Smooz',
+        'SM' => 'Snap Browser',
         'SN' => 'Snowshoe',
         'K5' => 'Spark',
         'B1' => 'Spectre Browser',
@@ -791,7 +792,7 @@ class Browser extends AbstractClientParser
             '2M', 'K7', '1N', '8A', 'H7', 'X3', 'T4', 'X4', '5O',
             '8C', '3M', '6I', '2P', 'PU', '7I', 'X5', 'AL', '3P',
             'W2', 'ZB', 'HN', 'Q6', 'Q7', 'G0', '00', 'R6', 'D8',
-            'PQ', 'LM', 'T5', '2N', 'SJ', 'X6',
+            'PQ', 'LM', 'T5', '2N', 'SJ', 'X6', 'SM',
         ],
         'Firefox'            => [
             'FF', 'BI', 'BF', 'BH', 'BN', 'C0', 'CU', 'EI', 'F1',
@@ -843,7 +844,7 @@ class Browser extends AbstractClientParser
         'DP', 'KL', 'K4', 'N6', 'KU', 'WK', 'M8', 'UP', 'ZT',
         '9P', 'N8', 'VR', 'N9', 'M9', 'F9', '0P', '0A', '2F',
         '2M', 'K7', '1N', '8A', 'H7', 'X3', 'X4', '5O', '6I',
-        '7I', 'X5', '3P', '2E', 'T5', '2N', 'SJ', 'X6',
+        '7I', 'X5', '3P', '2E', 'T5', '2N', 'SJ', 'X6', 'SM',
     ];
 
     /**
