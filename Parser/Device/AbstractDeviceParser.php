319b953ea660715c74ab9020bccc516ef078430e
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2025-12-30 02:53:20 +0100

    Rename Premier to Premier Star

--- Parser/Device/AbstractDeviceParser.php
+++ Parser/Device/AbstractDeviceParser.php
@@ -1494,7 +1494,7 @@ abstract class AbstractDeviceParser extends AbstractParser
         'PPD' => 'PPDS',
         'P3'  => 'PPTV',
         'FP'  => 'Premio',
-        'PR1' => 'Premier',
+        'PR1' => 'Premier Star',
         'PR'  => 'Prestigio',
         'P9'  => 'Primepad',
         'PRM' => 'PRIME',
