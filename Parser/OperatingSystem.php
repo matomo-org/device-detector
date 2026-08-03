b2242d3e24ad9a8fe39b9fea0654b63d4d73f356
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2026-08-03 19:17:31 +0200

    Improves detection for os client hints mapping (#8346)

--- Parser/OperatingSystem.php
+++ Parser/OperatingSystem.php
@@ -308,8 +308,9 @@ class OperatingSystem extends AbstractParser
      */
     protected static $clientHintMapping = [
         'GNU/Linux' => ['Linux'],
-        'Mac'       => ['MacOS'],
+        'Mac'       => ['MacOS', 'Mac OS X', 'MacIntel'],
         'Puffin OS' => ['Cloud Phone 2.4'],
+        'Windows'   => ['Win32'],
     ];
 
     /**
