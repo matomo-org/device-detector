3c31064fe6c0743800257632ddb5af96b9f28ea4
Liviu-Mihail Concioiu <liviu.concioiu@gmail.com>
2026-08-07 10:35:33 +0200

    Adds detection for Stadium

--- Parser/Client/Browser.php
+++ Parser/Client/Browser.php
@@ -653,6 +653,7 @@ class Browser extends AbstractClientParser
         'S3' => 'surf',
         '4S' => 'Surf Browser',
         'RY' => 'Surfy Browser',
+        '38' => 'Stadium',
         'SG' => 'Stargon',
         'T6' => 'Startpage',
         'S0' => 'START Internet Browser',
@@ -877,7 +878,7 @@ class Browser extends AbstractClientParser
         '2M', 'K7', '1N', '8A', 'H7', 'X3', 'X4', '5O', '6I',
         '7I', 'X5', '3P', '2E', 'T5', '2N', 'SJ', 'X6', 'SM',
         'AY', 'BQ', 'BC', 'NQ', 'VQ', 'KA', 'YS', 'D4', 'PZ',
-        'V7', 'D5', 'M0', 'Q8', 'T6', '37',
+        'V7', 'D5', 'M0', 'Q8', 'T6', '37', '38',
     ];
 
     /**
